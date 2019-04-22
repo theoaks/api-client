51d231bf1fb3c12765a33e2771bf6f533d8c935e
Babatunde.Ojerinde <ojerindej@theoaksoft.com>
2019-04-22 11:11:20 +0100

    Added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+
+declare module "api-client" {
+    export = ApiClient;
+}
+
+declare const ApiClient: ApiClientStatic;
+
+interface ApiResponse {
+    success: boolean,
+    message: string,
+    data: any,
+    metadata: any
+}
+
+interface ApiClientStatic {
+    new(baseUrl: string, authToken: string, publicKey: string): OaksEncryptorStatic;
+
+    send(
+        module: string,
+        action: string,
+        options: {
+            method: string,
+            data: any
+        }
+    ): Promise<ApiResponse>;
+
+    sendForm(
+        module: string,
+        action: string,
+        form: HTMLFormElement
+    ): Promise<ApiResponse>;
+}
